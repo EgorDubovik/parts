da1a4c4882013b7fa1d953ebb63ce0dab0074a8b
EgorDubovik <posik.web.m@gmail.com>
2020-10-08 22:23:59 -0500

    make template like admin

---
+++ resources/views/layout/left.blade.php
@@ -0,0 +1,34 @@
+<div id="layoutSidenav_nav">
+    <nav class="sidenav shadow-right sidenav-light">
+        <div class="sidenav-menu">
+            <div class="nav accordion" id="accordionSidenav">
+                <a class="nav-link" href="/dashboard">
+                    <div class="nav-link-icon">
+                      <i class="fas fa-align-center"></i>
+                    </div>
+                    Dashboard
+                </a>
+                <a class="nav-link" href="/fba">
+                    <div class="nav-link-icon">
+                      <i class="fas fa-truck-loading"></i>
+                    </div>
+                    FBA Сервис
+                </a>
+                <a class="nav-link" href="/fbm">
+                    <div class="nav-link-icon">
+                      <i class="fas fa-warehouse"></i>
+                    </div>
+                    FBM Сервис
+                </a>
+            </div>
+        </div>
+        <div class="sidenav-footer">
+            <div class="sidenav-footer-content">
+                <div class="sidenav-footer-subtitle">Logged in as:</div>
+                <div class="sidenav-footer-title">
+                   
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ resources/views/layout/top.blade.php
@@ -0,0 +1,27 @@
+
+<nav class="topnav navbar navbar-expand shadow navbar-light bg-white" id="sidenavAccordion">
+    <a class="navbar-brand" href="index.html">Warehouse admin</a>
+    <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 mr-lg-2" id="sidebarToggle" href="#"><i data-feather="menu"></i></button>
+   
+    <ul class="navbar-nav align-items-center ml-auto">
+        <li class="nav-item dropdown no-caret mr-3 dropdown-notifications">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownMessages" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="mail"></i></a>
+            <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownMessages">
+                <h6 class="dropdown-header dropdown-notifications-header">
+                    <i class="mr-2" data-feather="mail"></i>
+                    Message Center
+                </h6>
+                
+                <a class="dropdown-item dropdown-notifications-item">
+                   
+                    <div class="dropdown-notifications-item-content">
+                        <div class="dropdown-notifications-item-content-text">You don`t have new message</div>
+                        <div class="dropdown-notifications-item-content-details">Administration</div>
+                    </div>
+                </a>
+                <!-- <a class="dropdown-item dropdown-notifications-footer" href="#!">Read All Messages</a> -->
+            </div>
+        </li>
+        
+    </ul>
+</nav>
